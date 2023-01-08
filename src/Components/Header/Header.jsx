4a637c2c1bb45e14e7a0d7ab74e423a78c9cfd7c
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-01-08 19:04:08 +0900

    delete html

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
+import HeaderSrc from "../../images/header.jpg";
 
 const Header = () => {
   return (
@@ -9,7 +11,7 @@ const Header = () => {
       <header className="header">
         <Navbar />
         <div className="header-content flex-c flex text-center text-white">
-          <h2 className="header-title text-capitalize">GGanbu.GG</h2>
+          <HeaderTitle>GGanbu.GG</HeaderTitle>
           <br />
           <SearchForm />
         </div>
@@ -17,5 +19,20 @@ const Header = () => {
     </div>
   );
 };
+
+const HeaderContent = styled.div`
+  min-height: 60vh;
+  background: url(${HeaderSrc}) center/cover no-repeat;
+  flex-direction: column;
+  display: flex;
+  text-align: center;
+  color: white;
+  justify-content: center;
+`;
+
+const HeaderTitle = styled.div`
+  text-transform: capitalize;
+  font-size: 4.2rem;
+`;
 
 export default Header;
--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { FaSearch } from "react-icons/fa";
-import "./SearchForm.css";
+// import "./SearchForm.css";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const API_KEY = "RGAPI-56cd37e9-6c32-4a22-a6ae-918ec5321c1f"; //하루마다 바꿔야됨.
 
@@ -17,7 +18,6 @@ const SearchForm = () => {
       searchText +
       "?api_key=" +
       API_KEY;
-    //Handle the API Call
     axios
       .get(APICallString)
       .then(function (response) {
@@ -30,35 +30,44 @@ const SearchForm = () => {
       });
     navigate(`/profile/${searchText}`);
   }
-
-  // const onClickURL = () => {
-  //   navigate(`/profile/${searchText}`);
-  // };
   return (
-    <div className="search-form">
+    <SearchMain>
       <div className="container">
         <div className="search-form-content">
           <form className="search-form">
-            <div className="search-form-elem flex-sb flex bg-white">
+            <SearchElem>
               <input
                 type="text"
-                className="form-control text-black"
+                className=" p-2 text-3xl text-black"
                 placeholder="소환사 검색"
                 onChange={(e) => setSearchText(e.target.value)}
               />
               <button
                 type="submit"
-                className="flex-c flex"
+                className="flex justify-center"
                 onClick={(e) => searchForPlayer(e)}
               >
                 <FaSearch className="text-purple" size={32} />
               </button>
-            </div>
+            </SearchElem>
           </form>
         </div>
       </div>
-    </div>
+    </SearchMain>
   );
 };
+
+const SearchMain = styled.div`
+  width: 100%;
+  max-width: 680px;
+`;
+
+const SearchElem = styled.div`
+  padding: 0.8rem 2.8rem;
+  border-radius: 3.8rem;
+  display: flex;
+  background-color: white;
+  justify-content: space-between;
+`;
 
 export default SearchForm;
