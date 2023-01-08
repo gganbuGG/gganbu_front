7a6b4e0b76416e77eb814c1d484c53a12508dbef
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-01-08 22:21:02 +0900

    Board Update
    소환사명 link 추가

--- src/Components/Board/Td.jsx
+++ src/Components/Board/Td.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Td = ({ item }) => {
   return (
     <>
       <tr className="border-2 border-gray-200 bg-white">
         <td className="px-4 py-3 text-center">{item.rank}</td>
         <td className="px-4 py-3 text-center">
-          <a href="#">{item.name}</a>
+          <Link to={`/profile/${item.name}`}>{item.name}</Link>
         </td>
         <td className="px-4 py-3 text-center">{item.tier}</td>
         <td className="px-4 py-3 text-center">{item.LP}</td>
