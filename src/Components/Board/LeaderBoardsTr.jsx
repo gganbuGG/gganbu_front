9d69116eca2560e47fb5e366dc822572b5eceba1
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-01-14 23:21:29 +0900

    순위표 보드 추가

---
+++ src/Components/Board/LeaderBoardsTr.jsx
@@ -0,0 +1,13 @@
+import Td from "./Td";
+
+const Tr = ({ info }) => {
+  return (
+    <tbody>
+      {info.map((item) => {
+        return <Td key={item.id} item={item} />;
+      })}
+    </tbody>
+  );
+};
+
+export default Tr;
--- src/Routes/LeaderBoards/LeaderBoards.jsx
+++ src/Routes/LeaderBoards/LeaderBoards.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-import Board from "../../Components/Board/Board";
+import Board from "../../Components/Board/LeaderBoardsBoard";
 const LeaderBoards = () => {
   return (
     <main>
       <Navbar />
-      <div className="mt-24">
+      <div className="my-32">
         <Board />
       </div>
     </main>
