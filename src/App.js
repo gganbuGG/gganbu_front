60ecc6f4358580893fa4dfcfa4f76170d87f9453
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-01-18 15:27:34 +0900

    ScrollTop 생성

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import LeaderBoards from "./Routes/LeaderBoards/LeaderBoards";
 import Profile from "./Routes/Profile/Profile";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import ScrollTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <ScrollTop />
       <Routes>
         <Route path="/" exact={true} element={<Home />} />
         <Route path="/deck" exact={true} element={<Deck />} />
