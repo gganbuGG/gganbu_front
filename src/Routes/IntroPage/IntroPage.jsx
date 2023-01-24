33d2cbfa271a30f3a60470cb10d2e94fb415cddf
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-01-24 20:49:36 +0900

    intropage 수정 & secondbox 삭제

--- src/Routes/IntroPage/IntroPage.jsx
+++ src/Routes/IntroPage/IntroPage.jsx
@@ -1,14 +1,44 @@
 import AOS from "aos";
 import React from "react";
+
+import styled from "styled-components";
 import "./aos.css";
 
 export default function IntroPage() {
+  const redirectToGoogle = () => {
+    window.location.href = "/deck";
+    console.log("end");
+    console.log("end2");
+  };
   return (
-    <div>
-      <div className="container items-center justify-center">
-        <div>1</div>
-        <div>2</div>
-      </div>
-    </div>
+    <Wrapper>
+      <ProjectBox>
+        <button onClick={redirectToGoogle}>프로젝트 소개</button>
+      </ProjectBox>
+      <TeamBox>팀원 소개</TeamBox>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const ProjectBox = styled.div`
+  border: 3px solid white;
+  border-radius: 10px;
+  padding: 4px;
+  font-size: 60px;
+  font-weight: bold;
+`;
+
+const TeamBox = styled.div`
+  border: 3px solid white;
+  border-radius: 10px;
+  padding: 4px;
+  font-size: 60px;
+  font-weight: bold;
+`;
